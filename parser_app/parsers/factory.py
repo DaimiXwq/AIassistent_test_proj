987a174aa03119d1833f7db86b80dea5965931c2
Калинкин Дмитрий Витальевич <Kalinkin@sci.niimash>
2026-03-03 14:58:26 +0300

    Add ParserFactory Class

---
+++ parser_app/parsers/factory.py
@@ -0,0 +1,19 @@
+from .pdf_parser import PDFParser
+from .docx_parser import DOCXParser
+from .txt_parser import TXTParser
+from .cvs_parser import CSVParser
+
+class ParserFactory:
+
+    parsers = {
+        "pdf": PDFParser,
+        "docx": DOCXParser,
+        "txt": TXTParser,
+        "cvs": CSVParser,
+    }
+    @classmethod
+    def get_parser(cls, file_type):
+        parser = cls.parsers.get(file_type.lower())
+        if not parser:
+            raise ValueError("Unsupported format")
+        return parser()
