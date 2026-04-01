3fcc90bb4e51cd21ca23ce755e017108b63633c6
DaimiXwq <77454240+DaimiXwq@users.noreply.github.com>
2026-04-01 18:56:11 +0300

    Fix ParserFileSerializer import path in views

--- parser_app/views.py
+++ parser_app/views.py
@@ -3,7 +3,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ParserFileSerializer
+from .serializer import ParserFileSerializer
 from core.dispatcher import SourceDispatcher
 from db_server.services import DocumentService
 import tempfile
