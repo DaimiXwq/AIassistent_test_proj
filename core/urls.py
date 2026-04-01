8b8ff7ca809b08206a8a3678f57807b2eac22927
DaimiXwq <77454240+DaimiXwq@users.noreply.github.com>
2026-04-02 00:54:03 +0300

    Add manual test UI page for parser and search APIs

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from core.views import ChunkTextView, SearchView
+from core.views import ChunkTextView, ManualTestPageView, SearchView
 
 app_name = "core"
 
 urlpatterns = [
+    path("manual-test/", ManualTestPageView.as_view(), name="manual-test"),
     path("search/", SearchView.as_view(), name="search"),
     path("chunk/", ChunkTextView.as_view(), name="chunk"),
 ]
